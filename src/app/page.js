2e19d966b80f9c72519979bf4e8e648506c2a756
Doions <43123876+doions@users.noreply.github.com>
2025-03-11 10:20:36 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -202,7 +202,7 @@ export default function Home() {
       
 
         
-         DOIONS
+         DOIONS - the website is undergoing some server migration. you have caught us in the midst of a server transition.....
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
        Contact Now.
